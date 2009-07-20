961a155af7b1904b385653e18992aba9f9a02c9f
David Caudill <pyro2244@gmail.com>
2009-07-20 14:06:23 -0700

    Added Physics.cs this time. Oops.
    Signed-off-by: David Caudill <pyro2244@gmail.com>

---
+++ Emergence/Code/Physics/Physics.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Emergence
+{
+    public struct SymetMovmentUpdate
+    {
+        public Vector2 position;
+        public double rotation;
+        public Vector2 velocity;
+        public float angularVelocity;
+        public int worldID;
+        public List<HitUpdate> hits;
+
+        public SymetMovmentUpdate(int worldID, Vector2 position, double rotation, Vector2 velocity, float angularVelocity, List<HitUpdate> hits)
+        {
+            this.worldID = worldID;
+            this.position = position;
+            this.rotation = rotation;
+            this.velocity = velocity;
+            this.angularVelocity = angularVelocity;
+            this.hits = hits;
+        }
+    }
+
+    public struct HitUpdate
+    {
+        public int arm;
+        public int segment;
+        public SegmentType attacker;
+
+        public HitUpdate(int arm, int segment, SegmentType attacker)
+        {
+            this.arm = arm;
+            this.segment = segment;
+            this.attacker = attacker;
+        }
+    }
+
+    class Physics
+    {
+        public Physics()
+        {
+        }
+
+        public List<SymetMovmentUpdate> DoCollision(Dictionary<int, Symet> symets)
+        {
+            List<SymetMovmentUpdate> updates = new List<SymetMovmentUpdate>();
+
+            List<int> skipList = new List<int>();
+            foreach (Symet symet1 in symets.Values)
+            {
+                skipList.Add(symet1.WorldID);
+
+                foreach (Symet symet2 in symets.Values)
+                {
+                    if(skipList.Contains(symet2.WorldID))
+                        continue;
+
+                    List<LineHits> hits = PrimitiveShape.TestCollisionFull(symet1.Skeleton, symet1.WorldID, symet2.Skeleton, symet2.WorldID);
+                }
+            }
+
+            return updates;
+        }
+    }
+}
